7278c321f52bce5e86e58dc0e11fd88969fb8734
Justyna <justyna.str@wp.pl>
2018-02-10 14:03:13 +0100

    Home section on mobile with working mobile menu

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var menu_btn = document.getElementById('menu-button');
+var nav = document.getElementById('nav');
+var menu = document.getElementById('menu');
+
+
+
+menu_btn.addEventListener('click', function classToggle() {
+	menu.classList.toggle('menu');
+    menu.classList.toggle('menu-hidden');
+
+});
+
+
+
+
