dfb60e65f0b9648bbb75b2cf962c9ac36e4be7d4
Justyna <justyna.str@wp.pl>
2018-03-05 02:24:10 +0100

    added dropdown menu

--- js/script.js
+++ js/script.js
@@ -1,15 +1,44 @@
+
+
+
+
+
+/*
 var menu_btn = document.getElementById('menu-button');
-var nav = document.getElementById('nav');
-var menu = document.getElementById('menu');
 
-
-
-menu_btn.addEventListener('click', function classToggle() {
-	menu.classList.toggle('menu');
-    menu.classList.toggle('menu-opened');
-
+menu_btn.addEventListener('click', function (e){
+	
+	var menu = document.getElementById('menu');
+	
+	menu.classList.toggle( 'menu-opened' );
+	menu_btn.setAttribute( 'aria-expanded', String( menu.classList.contains( 'menu-opened' ) ) );
+	
+}, false);*/
+$(".menu-button").on('click', function(event) {
+	$(".menu-opened, .menu").toggleClass("menu-opened menu");
 });
 
+$("a").on('click', function(event) {
+	event.preventDefault();
 
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+
+  });
+
+/*
+var menu_btn = document.getElementById('menu-button');
+var nav = document.getElementById('nav');
+
+menu_btn.addEventListener('click', function (e){
+	
+	$(this).toggleClass('down');
+    $(nav).toggleClass('reveal');
+	
+	
+	
+}, false);
+*/
 
 
