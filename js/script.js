f04a44b36eb64ac19145ef3c108f22e6bbf4890e
Justyna <justyna.str@wp.pl>
2018-03-10 02:24:33 +0100

    Modified Project section and Offer section

--- js/script.js
+++ js/script.js
@@ -1,19 +1,5 @@
 
 
-
-
-
-/*
-var menu_btn = document.getElementById('menu-button');
-
-menu_btn.addEventListener('click', function (e){
-	
-	var menu = document.getElementById('menu');
-	
-	menu.classList.toggle( 'menu-opened' );
-	menu_btn.setAttribute( 'aria-expanded', String( menu.classList.contains( 'menu-opened' ) ) );
-	
-}, false);*/
 $(".menu-button").on('click', function(event) {
 	$(".menu-opened, .menu").toggleClass("menu-opened menu");
 });
@@ -22,23 +8,9 @@ $("a").on('click', function(event) {
 	event.preventDefault();
 
     $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
+        scrollTop: $($.attr(this, 'href')).offset().top - 30
+	
     }, 500);
 
   });
-
-/*
-var menu_btn = document.getElementById('menu-button');
-var nav = document.getElementById('nav');
-
-menu_btn.addEventListener('click', function (e){
-	
-	$(this).toggleClass('down');
-    $(nav).toggleClass('reveal');
-	
-	
-	
-}, false);
-*/
-
 
