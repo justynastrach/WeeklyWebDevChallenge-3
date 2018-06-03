3d1c6e60ff83c64f8b995518a7940570b31ae917
Justyna <justyna.str@wp.pl>
2018-06-03 13:17:46 +0200

    Fix some issues with open menu

--- js/script.js
+++ js/script.js
@@ -1,23 +1,52 @@
 $(document).ready(function(){
     $('.menu__element').click(function() {
-        $(this).siblings('li').removeClass('active');
+        $(this).children('li').removeClass('active');
         $(this).addClass('active');
     });
 });
 
+var test;
 
 $(".menu-button").on('click', function(event) {
-	$(".menu-opened, .menu").toggleClass("menu-opened menu");
+	test = $(".menu-opened, .menu").toggleClass("menu-opened menu");
+});
+
+
+
+$( window ).resize(function() {
+	if($( window ).width() > 992){
+		test.removeClass("menu-opened");
+		test.addClass("menu");
+	}
+
+
 });
 
 $("a").on('click', function(event) {
-	event.preventDefault();
-
+	console.log(this);
     $('html, body').animate({
+
         scrollTop: $($.attr(this, 'href')).offset().top - 30
 	
     }, 500);
 
   });
+
+$(window).scroll(function(){
+	var scroll = $(window).scrollTop();
+	
+	if(scroll>100){
+		$('section').each(function(i){
+			
+			if($(this).position().top < scroll ){
+	
+				$('.menu__element a.active').removeClass('active');
+				$('.menu__element a').eq(i).addClass('active');
+			}
+		})
+		
+		
+	}
+})
 
 
